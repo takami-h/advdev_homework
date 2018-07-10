95a64742d2edbdb490ca5b17a866cc1064cd91a9
Takami Hirata <takami-h@natswell.com>
2018-07-10 12:35:12 +0900

    sonarqube DONE

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,5 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+TEMPLATES_ROOT=$(dirname $0)/../templates
+echo oc new-app ${TEMPLATES_ROOT}/advdev-sonarqube-template.yml -n ${GUID}-sonarqube
